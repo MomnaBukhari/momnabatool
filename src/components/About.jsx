d163a75fc408e07cc3f36b9650005bb72e3b61fb
Momna Batool <smomnabatool@gmail.com>
2025-01-15 17:56:31 +0500

    About page content updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,44 +11,42 @@ function About() {
       <div className="about-page">
         <p>
           Console.log("Hello World! With a Dream to Serve in the World of
-          Innovation"); Wanna know my Vision? I am so hell interested in being
-          an ✧Artist✧. Artist??? Ain't You a Programmer?? I mean What's with ART
-          when you do just coding? Just Coding??? Just??? *Sigh* Introducing
-          myself as a ✧:･☽｡✧ Potato ✧:･.☽･ﾟ✧. Ops I am a Programmer, Precisely a
-          baby Programmer. ˖⁺‧₊˚✦ I just started coding. (It's been 2 years
-          practically, aah! and I have been in this field since 2020 aka 100
-          years ago) As a learner (ahem fighter 💪🏻) in Information Technology, I
-          said INFORMATION TECHNOLOGY... what a flex :), I find great excitement
-          in technology and am consistently amazed by its transformative impact
-          on the world. Certain skills I have learned in my Academia include
-          Data Structures and Algorithms (Cpp), C sharp, Database management,
-          and more. Spent more than 525960 minutes diving into the Core of
-          Software engineering (don’t ask about the seconds). In my final year,
-          I built a comprehensive Educational System for my project. That’s
-          where my love story with Web Engineering began. Fast forward to
-          mid-2024: I graduated (with the Gold medal! 🥇cheers ), but the
-          learning never stopped! Currently, I am learning Web specifically
-          practicing LARAVEL for the backend due to its dynamic and versatile
-          nature and database management in SQL. I have done various projects to
-          get hands-on experience. Sideways, I practice REACT for chemical
-          reactions. Ops, I mean for Front END, THE END, hehe. No!, not THE END,
-          I am still practicing React. Can you see my open IDE? Although I’m
-          still a baby Potato, I’m aiming to become a giant Ghost (like Casper,
-          but with cooler code). Sure, there are skill gaps, but my
-          determination fills them faster than a web page with lazy loading.
-          Beyond the world of technology, I am an avid tea buff and find myself
-          drawn to the captivating world of Graphic Arts. In my free time, I
-          like to color this world from my pallet and do web surfing, collecting
-          books and writing notes, and yeah saving ideas to develop later
-          [Later]. /* Updating my Skill-set every day, one byte at a time */
-          SKILLS summary (will update soon) • Programming: C++ | PHP •
-          Scripting: HTML | CSS • Front-end Libraries: Bootstrap, GSAP • Backend
-          Framework: Laravel • Database: MySQL • Core Concepts: OOP | Data
-          Structures | Software Engineering Let’s rewind to my vision: I want to
-          be a Web Engineer. [Big term, huh? Wish me luck!] Fun Fact: I
-          loveeeeeeeeeeee practicing Data Structures and algorithms and, of
-          course, ✦REACT✦ :))) Alright, time to get back to work. Have a good
-          day!
+          Innovation as a Web Engineer");
+          <br></br>
+          <br></br> I’m a Creative Developer driven by a passion for turning
+          innovative ideas into reality. I believe in coding as an art – where
+          creativity and logic comes together to craft masterpieces that make a
+          real difference. As a lifelong learner in the field of Information
+          Technology, I find great excitement in technology and am consistently
+          amazed by its transformative impact on the world.
+          <br></br>
+          <br></br>
+          I have been into this field since 2020, and throughout my
+          academic journey, I have developed a strong foundation in various
+          concepts like Object Oriented Programming, Data Structures and
+          Algorithms (C++), Database Management (SQL), Project Management, Web
+          Technologies and more. I’ve Spent more than 262800 minutes diving into
+          the Core concepts of Software engineering from requirement analysis to
+          deploying products. My passion for being web engineer ignited during
+          last year of my bachelor’s era when I developed a comprehensive Web
+          based Education System “StudySphere”, which was developed to fill the
+          gaps in existing systems. 
+          <br></br>
+          <br></br>
+          After graduating in 2024 (with a gold
+          medal!), I’ve been deepening my expertise by working on real-world
+          projects. I’m now expanding my skill set by learning the whole MERN
+          stack to enhance my ability to build fully integrated, end-to-end web
+          applications with JavaScript across both the client and server sides.
+          Moreover, I’m a firm believer in the Agile methodology and support its
+          iterative, collaborative approach. Despite any skill gaps, my
+          determination to learn compensates for them.
+          <br></br>
+          <br></br>Beyond the world of technology, I am an avid tea buff and
+          find myself drawn to the captivating world of Graphic Arts. In my free
+          time, I like to color this world from my pallet and do web surfing,
+          collecting books and writing notes and yes! exploring ideas to
+          developer later.
         </p>
       </div>
     </>
