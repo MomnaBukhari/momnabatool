018c50a65e2d8c1e07df3bc7d42e3366de3d7498
Momna Batool <smomnabatool@gmail.com>
2025-02-03 17:04:06 +0500

    Testing with mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RoutesComponent from "./components/Routes";
 import initializeCursor from "./scripting/cursor";
 import Footer from "./components/Footer";
+
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
   useEffect(() => {
     initializeCursor();
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <>
       <BrowserRouter>
         <div id="cursor"></div>
-        <Navbar />
-        <RoutesComponent />
-        <Footer/>
+        {isMobile ? (
+          <div className="mobile-message">
+            <img src="./images/char1.png"></img>
+            <h1>Hey! Sorry, Can you please switch to a Bigger Screen or Desktop View?</h1>
+            <p>Well, Smaller screen view development is in progress. <br></br> Thank you for your patience!</p>
+          </div>
+        ) : (
+          <>
+            <Navbar />
+            <RoutesComponent />
+            <Footer />
+          </>
+        )}
       </BrowserRouter>
     </>
   );
